1bbfcca01bc5b92945228d72dd675b649294e260
Nazim Fataliev <nazim.rabota@bk.ru>
2026-08-15 05:32:51 +0300

    test4

--- components/Navigation/HeaderSwitcher.jsx
+++ components/Navigation/HeaderSwitcher.jsx
@@ -6,41 +6,41 @@ import NProgress from "nprogress";
 import "nprogress/nprogress.css";
 
 export default function HeaderSwitcher() {
-    const [isAdmin, setIsAdmin] = useState(null);
+  //   const [isAdmin, setIsAdmin] = useState(null);
 
-    useEffect(()=>{
-      const tg = window.Telegram.WebApp;
-      const chatId =  tg?.initDataUnsafe?.user?.id;
+  //   useEffect(()=>{
+  //     const tg = window.Telegram.WebApp;
+  //     const chatId =  tg?.initDataUnsafe?.user?.id;
 
-    if (!chatId) {
-      setIsAdmin(false);
-      return;
-    }
-    NProgress.start();
-    fetch(`/api/user/admin`,{
-        headers: {
-    "X-User-ChatId": chatId.toString(),
-  },
-    })
-    .then(res=>res.json())
-    .then(data=>{
-        setIsAdmin(data.isAdmin);
-    })
-    .catch(() => {
-        setIsAdmin(false);
-      })
-    .finally(()=>{
-       NProgress.done();
-    })
-    },[]);
+  //   if (!chatId) {
+  //     setIsAdmin(false);
+  //     return;
+  //   }
+  //   NProgress.start();
+  //   fetch(`/api/user/admin`,{
+  //       headers: {
+  //   "X-User-ChatId": chatId.toString(),
+  // },
+  //   })
+  //   .then(res=>res.json())
+  //   .then(data=>{
+  //       setIsAdmin(data.isAdmin);
+  //   })
+  //   .catch(() => {
+  //       setIsAdmin(false);
+  //     })
+  //   .finally(()=>{
+  //      NProgress.done();
+  //   })
+  //   },[]);
       
-  if (isAdmin === null) {
+
   return (
         <>
        <AdminHeader /> 
        <Header />
       </> ) ;
-  }
+  
   
 
 }
